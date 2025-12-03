f1318236259afdbfd34366c31f0ca17d5c99b9b8
PSI <wldud5026@naver.com>
2025-12-03 13:35:55 +0900

    entity: UserEntity 클래스생성& 코드작성

---
+++ src/main/java/com/insightcrew/vo/UserEntity.java
@@ -0,0 +1,30 @@
+package com.insightcrew.vo;
+
+import java.time.LocalDateTime;
+
+import com.insightcrew.enums.UserRole;
+import com.insightcrew.enums.UserStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserEntity {
+	private Long pkId;
+	
+	private String id;
+	private String password;
+	private String name;
+	private String nickname;
+	
+	private UserRole role;
+	private UserStatus status;
+	
+	private LocalDateTime indate;
+	private String outdate;
+}
