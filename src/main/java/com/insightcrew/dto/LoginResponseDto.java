9be74ad9a748eaa3791fe6c460f549557f10f34b
PSI <wldud5026@naver.com>
2025-12-03 12:09:26 +0900

    login: responseDTO 코드작성

--- src/main/java/com/insightcrew/dto/LoginResponseDto.java
+++ src/main/java/com/insightcrew/dto/LoginResponseDto.java
@@ -1,5 +1,12 @@
 package com.insightcrew.dto;
 
-public class LoginResponseDto {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
+public class LoginResponseDto {
+	public boolean success;
+	private String id;
+	public String message;
 }
