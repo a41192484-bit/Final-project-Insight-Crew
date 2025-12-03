c2b9c092e4f9a4b8eee316884735b97f00392c4e
PSI <wldud5026@naver.com>
2025-12-03 16:43:31 +0900

    login: controller와UserRepositary 클래스 생성

---
+++ src/main/java/com/insightcrew/controller/LoginController.java
@@ -0,0 +1,5 @@
+package com.insightcrew.controller;
+
+public class LoginController {
+
+}
---
+++ src/main/java/com/insightcrew/repositary/UserRepositary.java
@@ -0,0 +1,7 @@
+package com.insightcrew.repositary;
+
+public class UserRepositary {
+	// dao. db에서 데이터를 가져오거나 db에 넣는 역할.
+	
+	// 로그인: 서비스에서 요청한 조건대로 클라이언트가 전달한 아이디
+}
