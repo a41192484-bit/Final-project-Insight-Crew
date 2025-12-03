6509f7e4aba7ed5a91bd319b81e115dc58999c0f
PSI <wldud5026@naver.com>
2025-12-03 12:40:44 +0900

    login: responseDTO에서public을private로수정/role 추가

--- src/main/java/com/insightcrew/dto/LoginResponseDto.java
+++ src/main/java/com/insightcrew/dto/LoginResponseDto.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class LoginResponseDto {
-	public boolean success;
+	private boolean success;
 	private String id;
-	public String message;
+	private String message;
+	private String role;
 }
