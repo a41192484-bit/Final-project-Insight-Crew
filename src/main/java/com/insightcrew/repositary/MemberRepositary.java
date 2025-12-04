c7426336d31ef7cbd7a262fbbc977b5d5ec212dd
PSI <wldud5026@naver.com>
2025-12-04 11:29:14 +0900

    클래스명 변경: UserRepositary -> MemberRepositary

--- src/main/java/com/insightcrew/repositary/MemberRepositary.java
+++ src/main/java/com/insightcrew/repositary/MemberRepositary.java
@@ -7,7 +7,7 @@ import com.insightcrew.domain.auth.dto.LoginRequestDto;
 import com.insightcrew.domain.member.vo.MemberVo;
 
 @Mapper
-public interface UserRepositary {
+public interface MemberRepositary {
 	// dao. db에서 데이터를 가져오거나 db에 넣는 역할.
 	
 	// 아이디 찾기
