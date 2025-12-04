cace6b6d70ce21a9bc5be3da8da9e3bfa99f651a
PSI <wldud5026@naver.com>
2025-12-04 11:44:02 +0900

    service: class name mod. UserService -> MemberService

---
+++ src/main/java/com/insightcrew/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.insightcrew.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.insightcrew.dto.LoginRequestDto;
+
+@Service
+public class MemberService {
+	
+}
