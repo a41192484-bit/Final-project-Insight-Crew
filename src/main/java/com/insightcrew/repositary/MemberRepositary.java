9ada68aff7c43085ab6336898dbbce3038f41abe
PSI <wldud5026@naver.com>
2025-12-05 14:02:28 +0900

    join: repositary 코드작성

--- src/main/java/com/insightcrew/repositary/MemberRepositary.java
+++ src/main/java/com/insightcrew/repositary/MemberRepositary.java
@@ -1,15 +1,20 @@
 package com.insightcrew.repositary;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import com.insightcrew.domain.member.vo.MemberVo;
 
 
 @Mapper
 public interface MemberRepositary {
-	void save(MemberVo vo);
+	Long save(MemberVo vo);
 	
+	//아이디로 회원 조회
 	MemberVo findByUserid(String userid);
+	
+	//닉네임 중복 조회
 	MemberVo findByNickname(String nickname);
+
+	//회원저장
+	int insertMamber(MemberVo vo);
 }
