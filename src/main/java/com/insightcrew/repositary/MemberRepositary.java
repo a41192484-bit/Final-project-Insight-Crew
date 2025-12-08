295b1afc37047271085a1941037b3ad8f3c0ab2d
PSI <wldud5026@naver.com>
2025-12-08 12:45:46 +0900

    join: repositary 주석 수정, 회원가입 int를 void로 수정

--- src/main/java/com/insightcrew/repositary/MemberRepositary.java
+++ src/main/java/com/insightcrew/repositary/MemberRepositary.java
@@ -7,14 +7,13 @@ import com.insightcrew.domain.member.vo.MemberVo;
 
 @Mapper
 public interface MemberRepositary {
-	//아이디로 회원 조회
+	
+	//아이디 조회
 	MemberVo findByUserid(String userid);
 	
-	//닉네임 중복 체크
+	//닉네임 조회
 	MemberVo findByNickname(String nickname);
 	
-	//아이디 중복체크
-
 	//회원가입
-	int insertMember(MemberVo vo);
+	void insertMember(MemberVo membervo);
 }
