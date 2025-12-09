8d776e247f76a8609dca030a4ffb11823427e675
PSI <wldud5026@naver.com>
2025-12-09 17:42:51 +0900

    mypage: controller코드 작성

--- src/main/java/com/insightcrew/controller/MypageController.java
+++ src/main/java/com/insightcrew/controller/MypageController.java
@@ -4,15 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequestMapping("/mypage")
 public class MypageController {
+	
 	//마이페이지 뷰
 	@GetMapping("/mypage")
 	public String mypage() {
 		return "member/member-mypage";
 	}
-	//회원정보관리 이동
+	
+	//회원정보관리
+	@GetMapping("/member")
+	public String memberInfo() {
+		return "member/member-info";
+	}
 	
 	//로그아웃
 	@GetMapping("/logout")
@@ -21,4 +28,9 @@ public class MypageController {
 	}
 	
 	//회원탈퇴
+	@GetMapping("/inactive")
+	public String inactive() {
+		return new String();
+	}
+	
 }
