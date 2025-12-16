ee9a9a5d0cb1fa04bb155b7e06d2e0ef15a6cbb7
CSI <a41192484@gmail.com>
2025-12-16 15:10:22 +0900

    config 수정

--- src/main/java/com/insightcrew/config/MyBatisConfig.java
+++ src/main/java/com/insightcrew/config/MyBatisConfig.java
@@ -3,8 +3,8 @@ package com.insightcrew.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
-// MyBatis 전용 설정 (mapper 경로 지정, typeAliases, camelCase 옵션 등)
 @Configuration
 @MapperScan(basePackages = "com.insightcrew.repository")
 public class MyBatisConfig {
+	
 }
--- src/main/java/com/insightcrew/config/WebMvcConfig.java
+++
@@ -1,6 +0,0 @@
-package com.insightcrew.config;
-
-// 인터셉터, 정적 리소스, MVC 전역 설정 (웹 요청 처리 전역 설정)
-public class WebMvcConfig {
-
-}
